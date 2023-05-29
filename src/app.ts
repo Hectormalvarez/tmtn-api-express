43ab7b04d18e7d7b2a351e9a863c52e8d44bfe1f
Hector Alvarez <alvarez.hectorm@outlook.com>
2023-05-29 06:12:18 -0500

    product route done

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ import connect from "./utils/connect";
 import logger from "./utils/logger";
 import routes from "./routes";
 import deserializeUser from "./middlewares/deserializeUser";
-import requireUser from "./middlewares/requireUser";
 
 const port = config.get<number>('port')
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,17 @@
 import { Express, Request, Response } from "express"
 
-import { createUserSessionHandler, getUserSessionsHandler, deleteSessionHandler } from "./controllers/session.controller"
+import {
+    createUserSessionHandler,
+    getUserSessionsHandler,
+    deleteSessionHandler
+} from "./controllers/session.controller"
 import { createUserHandler } from "./controllers/user.controller"
+import { createProductHandler, deleteProductHandler, getProductHandler, updateProductHandler } from "./controllers/product.controller"
 import validateResource from "./middlewares/validateResource"
+import requireUser from "./middlewares/requireUser"
 import { createSessionSchema } from "./schemas/session.schema"
 import { createUserSchema } from "./schemas/user.schema"
-import requireUser from "./middlewares/requireUser"
+import { createProductSchema, deleteProductSchema, getProductSchema, updateProductSchema } from "./schemas/product.schema"
 
 function routes(app: Express) {
     app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200))
@@ -17,6 +23,30 @@ function routes(app: Express) {
     app.get('/api/sessions', requireUser, getUserSessionsHandler)
 
     app.delete('/api/sessions', requireUser, deleteSessionHandler)
+
+    app.post(
+        "/api/products",
+        [requireUser, validateResource(createProductSchema)],
+        createProductHandler
+    );
+
+    app.put(
+        "/api/products/:productId",
+        [requireUser, validateResource(updateProductSchema)],
+        updateProductHandler
+    );
+
+    app.get(
+        "/api/products/:productId",
+        validateResource(getProductSchema),
+        getProductHandler
+    );
+
+    app.delete(
+        "/api/products/:productId",
+        [requireUser, validateResource(deleteProductSchema)],
+        deleteProductHandler
+    );
 }
 
 export default routes
